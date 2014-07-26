31d3d60018de439f85f82946a48908ea4747f19b
WillWhart <WillWhart@users.noreply.github.com>
2014-07-26 11:01:23 -0700

    Update cachematrix.R
    First commit. Will complete later

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,11 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+	## compare matrix to already loaded matrices
+	## If it has been computed, load the pre-computed inverse
+	## else compute the matrix, and load it and inverse 
+	## return this inverse
+
 
 }
 
@@ -12,4 +17,5 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	## Call compute inverse
 }
